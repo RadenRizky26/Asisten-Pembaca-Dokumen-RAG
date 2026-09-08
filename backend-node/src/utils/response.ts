a7a763445f21e3957415c0c7a3d533d2c5ff22a0
Raden Rizky <radenr775@gmail.com>
2026-09-08 08:30:59 +0700

    fix: backend

--- backend-node/src/utils/response.ts
+++ backend-node/src/utils/response.ts
@@ -1,8 +1,8 @@
 import type { Response } from 'express';
 
-export const ok = (res: Response, data: unknown, msg = 'OK') => res.json({ success: true, message: msg, data });
-export const created = (res: Response, data: unknown, msg = 'Created') => res.status(201).json({ success: true, message: msg, data });
-export const badRequest = (res: Response, msg = 'Bad request') => res.status(400).json({ success: false, message: msg });
-export const unauthorized = (res: Response, msg = 'Unauthorized') => res.status(401).json({ success: false, message: msg });
-export const notFound = (res: Response, msg = 'Not found') => res.status(404).json({ success: false, message: msg });
-export const fail = (res: Response, msg = 'Internal error', code = 500) => res.status(code).json({ success: false, message: msg });
+export const ok = (res: Response, data: unknown) => res.json(data);
+export const created = (res: Response, data: unknown) => res.status(201).json(data);
+export const badRequest = (res: Response, detail = 'Bad request') => res.status(400).json({ detail });
+export const unauthorized = (res: Response, detail = 'Unauthorized') => res.status(401).json({ detail });
+export const notFound = (res: Response, detail = 'Not found') => res.status(404).json({ detail });
+export const fail = (res: Response, detail = 'Internal error', code = 500) => res.status(code).json({ detail });
